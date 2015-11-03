a3a3d4bd2e742b4f202e2e6e2dcd9484683b7776
Rafael Motta <rafaelmotta021@gmail.com>
2015-11-03 10:34:12 -0200

    Api company

---
+++ src/api/companyApi.js
@@ -0,0 +1,12 @@
+let api = (Restangular) => {
+  return new class CompanyApi {
+    fetchBySubdomain(company) {
+      return Restangular
+        .one('companies', company.subdomain)
+        .get();
+    }
+  }
+};
+
+angular.module('store.api.client.foodio').factory('companyApi', api);
+api.$inject = ['Restangular'];
--- store-api-client-foodio.js
+++ store-api-client-foodio.js
@@ -39,6 +39,31 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
+var api = function api(Restangular) {
+  return new ((function () {
+    function CompanyApi() {
+      _classCallCheck(this, CompanyApi);
+    }
+
+    _createClass(CompanyApi, [{
+      key: 'fetchBySubdomain',
+      value: function fetchBySubdomain(company) {
+        return Restangular.one('companies', company.subdomain).get();
+      }
+    }]);
+
+    return CompanyApi;
+  })())();
+};
+
+angular.module('store.api.client.foodio').factory('companyApi', api);
+api.$inject = ['Restangular'];
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
 var api = function api(Restangular, ApiBase) {
   return new ((function () {
     function ConfirmationApi() {
