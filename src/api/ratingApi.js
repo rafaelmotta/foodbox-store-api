0550be1ff8dfdebdac03c5718484cc8075fd7925
Rafael Motta <rafaelmotta021@gmail.com>
2015-11-09 18:12:27 -0200

    Rating api

---
+++ src/api/ratingApi.js
@@ -0,0 +1,37 @@
+let api = (Restangular, ApiBase) => {
+  return new class CostumerApi extends ApiBase {
+    fetch(order) {
+      return Restangular
+        .one('companies', this.company.id)
+        .one('costumers', this.costumer.id)
+        .one('orders', order.id)
+        .one('ratings', order.rating.id)
+        .get();
+    }
+
+    create(rating) {
+      this._beforeCreate(rating, (ratingSerialized) => {
+        return Restangular
+          .one('companies', this.company.id)
+          .one('costumers', this.costumer.id)
+          .one('orders', rating.order.id)
+          .post('ratings', { rating: ratingSerialized });
+      });
+    }
+
+    _beforeCreate(data) {
+      return $q((resolve, reject) => {
+        resolve({
+          delivery: data.delivery,
+          score: data.score,
+          quality: data.quality,
+          good_comment: data.goodComment,
+          bad_comment: data.bad_comment
+        });
+      });
+    }
+  }
+};
+
+angular.module('store.api.client.foodio').factory('ratingApi', api);
+api.$inject = ['Restangular', 'ApiBase'];
--- store-api-client-foodio.js
+++ store-api-client-foodio.js
@@ -467,6 +467,61 @@ function _inherits(subClass, superClass) { if (typeof superClass !== 'function' 
 
 var api = function api(Restangular, ApiBase) {
   return new ((function (_ApiBase) {
+    _inherits(CostumerApi, _ApiBase);
+
+    function CostumerApi() {
+      _classCallCheck(this, CostumerApi);
+
+      _get(Object.getPrototypeOf(CostumerApi.prototype), 'constructor', this).apply(this, arguments);
+    }
+
+    _createClass(CostumerApi, [{
+      key: 'fetch',
+      value: function fetch(order) {
+        return Restangular.one('companies', this.company.id).one('costumers', this.costumer.id).one('orders', order.id).one('ratings', order.rating.id).get();
+      }
+    }, {
+      key: 'create',
+      value: function create(rating) {
+        var _this = this;
+
+        this._beforeCreate(rating, function (ratingSerialized) {
+          return Restangular.one('companies', _this.company.id).one('costumers', _this.costumer.id).one('orders', rating.order.id).post('ratings', { rating: ratingSerialized });
+        });
+      }
+    }, {
+      key: '_beforeCreate',
+      value: function _beforeCreate(data) {
+        return $q(function (resolve, reject) {
+          resolve({
+            delivery: data.delivery,
+            score: data.score,
+            quality: data.quality,
+            good_comment: data.goodComment,
+            bad_comment: data.bad_comment
+          });
+        });
+      }
+    }]);
+
+    return CostumerApi;
+  })(ApiBase))();
+};
+
+angular.module('store.api.client.foodio').factory('ratingApi', api);
+api.$inject = ['Restangular', 'ApiBase'];
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var api = function api(Restangular, ApiBase) {
+  return new ((function (_ApiBase) {
     _inherits(RegistrationApi, _ApiBase);
 
     function RegistrationApi() {
