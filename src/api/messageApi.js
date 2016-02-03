664d24b6c5472f5822025303c59c5f2d54bebf11
Rafael Motta <rafaelmotta021@gmail.com>
2016-02-03 11:20:21 -0200

    Message api

---
+++ src/api/messageApi.js
@@ -0,0 +1,13 @@
+let api = (Restangular, ApiBase) => {
+  return new class StoreApi extends ApiBase {
+    show(data) {
+      return Restangular
+        .one('companies', this.company.id)
+        .one('stores', this.store.id)
+        .one('messages', data.page)
+    }
+  }
+};
+
+angular.module('store.api.client.foodio').factory('messageApi', api);
+api.$inject = ['Restangular', 'ApiBase'];
--- store-api-client-foodio.js
+++ store-api-client-foodio.js
@@ -458,6 +458,39 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var api = function api(Restangular, ApiBase) {
+  return new ((function (_ApiBase) {
+    _inherits(StoreApi, _ApiBase);
+
+    function StoreApi() {
+      _classCallCheck(this, StoreApi);
+
+      _get(Object.getPrototypeOf(StoreApi.prototype), 'constructor', this).apply(this, arguments);
+    }
+
+    _createClass(StoreApi, [{
+      key: 'show',
+      value: function show(data) {
+        return Restangular.one('companies', this.company.id).one('stores', this.store.id).one('messages', data.page);
+      }
+    }]);
+
+    return StoreApi;
+  })(ApiBase))();
+};
+
+angular.module('store.api.client.foodio').factory('messageApi', api);
+api.$inject = ['Restangular', 'ApiBase'];
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var api = function api($q, Restangular, ApiBase) {
   return new ((function (_ApiBase) {
     _inherits(OrderApi, _ApiBase);
